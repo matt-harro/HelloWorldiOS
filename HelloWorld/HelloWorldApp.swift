da00adb38d43bca84b78504d6f5d0d6c95ef6820
Matt Harro <111941302+matt-harro@users.noreply.github.com>
2024-12-30 14:09:32 +1000

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Matthew Harrison on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
